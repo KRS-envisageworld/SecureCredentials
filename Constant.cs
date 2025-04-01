a5a7e930711a5f429c3304c19e869179d8eb2218
Kartik Sudra <83295488+KRS-envisageworld@users.noreply.github.com>
2025-04-02 01:52:50 +0530

    Update Constant.cs

--- Constant.cs
+++ Constant.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace SecureCredentials
 {
-	internal class Constant
-	{
-	}
+    /// <summary>
+    /// Contains constant values used for secure credential management used for password hashing.
+    /// </summary>
+    public class Constant
+    {
+        /// <summary>
+        /// The size of key to derive.
+        /// </summary>
+        public const int KEYSIZE = 64;
+        
+        /// <summary>
+        /// Salt size
+        /// </summary>
+        public const int SALTSIZE = 16;
+        
+        /// <summary>
+        /// The number of iterations to derive the key.
+        /// </summary>
+        public const int ITERATIONS = 3_50_000;
+
+        /// <summary>
+        /// Algorithm type to hash the password.
+        /// </summary>
+        public static readonly HashAlgorithmName HASHALGORITHM = HashAlgorithmName.SHA512;
+
+    }
 }
